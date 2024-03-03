db4974a8914872725dd265bfc871f7f500b52e51
Ronaldo <r2desen@gmail.com>
2024-03-03 14:52:29 -0300

    turned pet card into a JS template

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const template = document.querySelector("#pet-card-template");
-const wrapper = document.creatDocumentFragment();
+const wrapper = document.createDocumentFragment();
 
 async function start() {
   const weatherPromisse = await fetch(
@@ -7,7 +7,7 @@ async function start() {
   );
   const weatherData = await weatherPromisse.json();
   const ourTemperature = weatherData.properties.periods[0].temperature;
-  document.querySelector("#temperature-outpur").textContent = ourTemperature;
+  document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
 start();
@@ -19,8 +19,10 @@ async function petsArea() {
   const petsData = await petsPromise.json();
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true);
-    wrapper.appendChild(clone)
+    clone.querySelector("h3").textContent = pet.name;
+    wrapper.appendChild(clone);
   });
+  document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 
 petsArea();
